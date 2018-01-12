f26b1788ec5cbd86b806ca6e8c6662a55310dc9f
Alex Ling <alex.yy.ling@gmail.com>
2018-01-12 15:52:59 -0600

    Added things

---
+++ src/org/discobots/powerup/subsystems/Intake.java
@@ -0,0 +1,11 @@
+package org.discobots.powerup.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Intake extends Subsystem {
+	
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+}
