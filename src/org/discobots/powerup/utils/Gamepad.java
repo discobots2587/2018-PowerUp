5a64df2a1470aade11f5132b6984e77f1e4e84a6
Alex Ling <alex.yy.ling@gmail.com>
2018-01-18 18:01:57 -0600

    Gamepad Updates

--- src/org/discobots/powerup/utils/Gamepad.java
+++ src/org/discobots/powerup/utils/Gamepad.java
@@ -6,6 +6,30 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class Gamepad extends Joystick {
 	
+	public static int AXIS_LX = 0;
+	public static int AXIS_LY = 1;
+	public static int AXIS_RX = 2;
+	public static int AXIS_RY = 3;
+	
+	public static int DPAD_X = 4;
+	public static int DPAD_Y = 5;
+	
+	public static int BTN_X = 1;
+	public static int BTN_A = 2;
+	public static int BTN_B = 3;
+	public static int BTN_Y = 4;
+
+	public static int BTN_LB = 5;
+	public static int BTN_LT = 7;
+	public static int BTN_RB = 6;
+	public static int BTN_RT = 8;
+
+	public static int BTN_BACK = 9;
+	public static int BTN_START = 10;
+
+	public static int AXISBTN_L = 11;
+	public static int AXISBTN_R = 12;
+	
 	private static final int DEFAULT_USB_PORT = 1;
 	
 	public Gamepad() {
@@ -14,5 +38,29 @@ public class Gamepad extends Joystick {
 	
 	public Gamepad(int port) {
 		super(port);
+	}
+	
+	public double getLX() {
+		return this.getRawAxis(AXIS_LX);
+	}
+
+	public double getLY() {
+		return this.getRawAxis(AXIS_LY) * -1;
+	}
+
+	public double getRX() {
+		return this.getRawAxis(AXIS_RX);
+	}
+
+	public double getRY() {
+		return this.getRawAxis(AXIS_RY) * -1;
+	}
+
+	public double getDX() {
+		return this.getRawAxis(DPAD_X);
+	}
+
+	public double getDY() {
+		return this.getRawAxis(DPAD_Y) * -1;
 	}
 }
