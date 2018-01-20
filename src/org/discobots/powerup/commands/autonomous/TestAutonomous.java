c6962cc0cfd2f874c0578cfd59a2f90172157c4d
Alex Ling <alex.yy.ling@gmail.com>
2018-01-19 18:00:33 -0600

    TestAuton

---
+++ src/org/discobots/powerup/commands/autonomous/TestAutonomous.java
@@ -0,0 +1,13 @@
+package org.discobots.powerup.commands.autonomous;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class TestAutonomous extends Command {
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
