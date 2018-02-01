f87faa0bc5ab3e2d7dc9482ae8e67cafc5935a24
Alex Ling <alex.yy.ling@gmail.com>
2018-02-01 16:40:50 -0600

    Minor Edits

--- src/org/discobots/powerup/Autonomous.java
+++ src/org/discobots/powerup/Autonomous.java
@@ -1,5 +1,13 @@
 package org.discobots.powerup;
 
+import edu.wpi.first.wpilibj.DriverStation;
+
 public class Autonomous {
 
+	public static String gameData;
+	
+	public static void init() {
+		gameData = DriverStation.getInstance().getGameSpecificMessage();
+	}
+	
 }
--- src/org/discobots/powerup/Dashboard.java
+++ src/org/discobots/powerup/Dashboard.java
@@ -7,20 +7,33 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Dashboard {
 	
+	private static final double SHORT_DELAY = 0.25;
+	private static final double LONG_DELAY = 0.5;
+	
+	private static double shortTime = 0.0;
+	private static double longTime = 0.0;
+	
 	public static void init() {
 	}
 	
+	//Short & Long dashboard update based of off 3847's dashboard
 	public static void update() {
-		updateLong();
-		updateShort();
-	}
-	
-	public static void updateLong() {
-		
+		if((Timer.getFPGATimestamp() - shortTime) > SHORT_DELAY) {
+			shortTime = Timer.getFPGATimestamp();
+			updateShort();
+		}
+		if((Timer.getFPGATimestamp() - longTime) > LONG_DELAY) {
+			longTime = Timer.getFPGATimestamp();
+			updateLong();
+		}
 	}
 	
 	public static void updateShort() {
 		SmartDashboard.putNumber("Match Time", Timer.getMatchTime());
 		SmartDashboard.putBoolean("Launcher Ready?", !(Robot.launcher.checkOnCooldown()||Robot.launcher.anyActivated()));
+	}
+	
+	public static void updateLong() {
+		
 	}
 }
