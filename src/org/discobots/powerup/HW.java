3328f95522d1293ceca809f25535fd69d5d30492
Tom Yarrow <tyarrow1@gmail.com>
2018-02-21 08:24:57 -0600

    late night changes

--- src/org/discobots/powerup/HW.java
+++ src/org/discobots/powerup/HW.java
@@ -20,7 +20,7 @@ public class HW {
 	public static int rightIntake = 1;
 	public static int leftIntake  = 2;
 	public static int armMotor    = 3;
-	public static int winch		  = 4;
+	public static int winch		  = 5;
 	
 	//CAN ports
 	public static int talonsrx_pigeon = 35; //CAN id
